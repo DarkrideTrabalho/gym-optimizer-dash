c2b6be2e0976e727b4573593f033b6bd5931aecc
DarkrideTrabalho <contatrabalhodark@gmail.com>
2025-04-11 14:47:52 +0100

    bloco

--- supabase/functions/generate-optimal-schedule/index.ts
+++ supabase/functions/generate-optimal-schedule/index.ts
@@ -56,16 +56,8 @@ const teachers: { [key: string]: Teacher } = {
 
 const days = ["Segunda", "Terça", "Quarta", "Quinta", "Sexta"];
 const timeSlots = [
-  "10:00 - 11:00",
-  "10:30 - 11:30",
-  "16:00 - 17:00",
-  "16:30 - 17:30",
-  "17:00 - 18:00",
-  "17:30 - 18:30",
-  "18:00 - 19:00",
-  "18:30 - 19:30",
-  "19:00 - 20:00",
-  "19:30 - 20:30",
+  "10:00 - 11:30",
+  "16:00 - 20:30",
 ];
 
 // Função para calcular a popularidade das aulas
